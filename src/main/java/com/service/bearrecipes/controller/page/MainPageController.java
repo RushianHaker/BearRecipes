c51a61e83a2611be537323d1721b4a7b3a0801ac
Maxim Ivanov <m.ivanov@r77.center-inform.ru>
2023-11-29 00:05:39 +0300

    нейминг

--- src/main/java/com/service/bearrecipes/controller/page/MainPageController.java
+++ src/main/java/com/service/bearrecipes/controller/page/MainPageController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
-public class MenuPageController {
+public class MainPageController {
   @GetMapping({ "/" })
   public String getMenuPage() {
-    return "menu";
+    return "main";
   }
 }
