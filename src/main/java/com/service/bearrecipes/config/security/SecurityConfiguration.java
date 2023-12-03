acb8eacd014eb36b84fe9d5b20d36e6c0a90457c
Max Ivanov <mamalenaralalend@gmail.com>
2023-12-04 01:21:50 +0300

    SecurityConfiguration

--- src/main/java/com/service/bearrecipes/config/security/SecurityConfiguration.java
+++ src/main/java/com/service/bearrecipes/config/security/SecurityConfiguration.java
@@ -10,23 +10,50 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebSecurity
 @Configuration
-public class SecurityConfiguration {
+public class SecurityConfiguration implements WebMvcConfigurer {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.sessionManagement(sessionManagement ->
                         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
                 .authorizeHttpRequests(request ->
-                        request.requestMatchers("/").permitAll()
-                                .requestMatchers("/main").hasAnyRole("ADMIN", "USER")
-                                .requestMatchers("/admin/main").hasRole("ADMIN")
+                        request.requestMatchers(
+                                        "/",
+                                        "/user/registration",
+                                        "/error").permitAll()
+                                .requestMatchers(
+                                        "/logout",
+                                        "/receipt",
+                                        "/receipt/inforeceipt/{receiptId}",
+                                        "/order",
+                                        "/order/createorder",
+                                        "/user/profileuser").hasAnyRole("ADMIN", "USER", "DEV")
+                                .requestMatchers(
+                                        "/receipt/addreceipt",
+                                        "/receipt/editreceipt/{receiptId}",
+                                        "/receipt/delreceipt/{receiptId}",
+                                        "/ingredient/addingredient/{receiptId}",
+                                        "/ingredient/deleteingredient/{ingredientId}",
+                                        "/stepinfo/addstep/{receiptId}",
+                                        "/stepinfo/deletestep/{stepInfoId}").hasAnyRole("ADMIN", "DEV")
+                                .requestMatchers(
+                                        "/v3/api-docs/**",
+                                        "/swagger-ui/**",
+                                        "/swagger-ui.html").hasRole("DEV")
                                 .anyRequest()
                                 .authenticated())
                 .formLogin(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .build();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
     }
 
     @SuppressWarnings("deprecation")
